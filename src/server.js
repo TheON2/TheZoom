44ce9780ef6324597bce6222f29966e1af1c9af6
TheON2 <sniperad@naver.com>
2023-06-20 13:47:40 +0900

    Update : adminUI

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 // import {WebSocket} from "ws";
-import SocketIO from "socket.io";
+import { instrument} from "@socket.io/admin-ui";
+import {Server} from "socket.io";
 import http from "http";
 
 const app = express();
@@ -14,7 +15,15 @@ app.get("/*", (_,res) => res.redirect("/"));
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer);
+const wsServer = new Server(httpServer,{
+  cors:{
+    origin: ['https://admin.socket.io'],
+    credentials:true,
+  }
+});
+instrument(wsServer,{
+  auth:false
+})
 
 function publicRooms(){
   const {sockets: {adapter: {sids, rooms}}} = wsServer;
