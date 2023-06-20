0fe46626edd4fd83fe1c84a40966d4a46aa4ed4d
TheON2 <sniperad@naver.com>
2023-06-20 17:09:59 +0900

    Update : choose Camera

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -9,29 +9,40 @@ let myStream;
 let mic = true;
 let camera = true;
 
-async function getMedia(){
+async function getMedia(deviceId){
+  const initialConstrains = {
+    audio: true,
+    video: { facingMode: 'user'}
+  }
+  const cameraConstrains = {
+    audio: true,
+    video: { deviceId: {exact: deviceId}}
+  }
   try {
-    myStream = await navigator.mediaDevices.getUserMedia({
-      audio:true,
-      video:true,
-    })
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId? cameraConstrains : initialConstrains
+    )
     myFace.srcObject = myStream
+    if(!deviceId) await getCameras()
   } catch (e){
     console.log(e)
   }
 }
 
 getMedia()
-getCameras()
 
 async function getCameras(){
   try{
     const devices = await navigator.mediaDevices.enumerateDevices()
     const cameras = devices.filter(device=>device.kind === 'videoinput')
+    const currentCamera = myStream.getVideoTracks()[0]
     cameras.forEach(camera => {
       const option = document.createElement('option')
       option.value = camera.deviceId
       option.innerText = camera.label
+      if (currentCamera.label === camera.label) {
+        option.selected = true
+      }
       cameraSelect.appendChild(option);
     })
   }catch (e){console.log(e)}
@@ -62,5 +73,10 @@ function handleCameraClick(){
   }
 }
 
+async function handleCameraChange(){
+  await getMedia(cameraSelect.value)
+}
+
 muteBtn.addEventListener("click",handleMuteClick)
 cameraBtn.addEventListener("click",handleCameraClick)
+cameraSelect.addEventListener("input",handleCameraChange)
