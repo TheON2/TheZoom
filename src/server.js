b293110910428fb0e2741de158a49373837b6872
TheON2 <sniperad@naver.com>
2023-06-21 12:40:25 +0900

    Fix : views pug change for convert html

--- src/server.js
+++ src/server.js
@@ -2,13 +2,15 @@ import express from "express";
 // import {WebSocket} from "ws";
 import SocketIO from "socket.io";
 import http from "http";
+import path from "path";
+
 
 const app = express();
 
-app.set("view engine","pug");
+// app.set("view engine","pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (_, res) => res.render("home"));
+app.get("/", (_, res) => res.sendFile(path.join(__dirname, "/views/index.html")));
 app.get("/*", (_,res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
