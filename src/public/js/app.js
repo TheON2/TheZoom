2c9f924d031fcdb7f4b11b371179d0a6a6957051
TheON2 <sniperad@naver.com>
2023-06-21 09:27:34 +0900

    Update : localtunnel

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -63,6 +63,7 @@ function handleMuteClick(){
     mic = true
   }
 }
+
 function handleCameraClick(){
   myStream
     .getVideoTracks()
@@ -78,6 +79,14 @@ function handleCameraClick(){
 
 async function handleCameraChange(){
   await getMedia(cameraSelect.value)
+  if (myPeerConnection){
+    const videoTrack = myStream.getVideoTracks()[0]
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => sender.track.kind === 'video')
+    console.log(videoSender)
+    videoSender.replaceTrack(videoTrack)
+  }
 }
 
 muteBtn.addEventListener("click",handleMuteClick)
